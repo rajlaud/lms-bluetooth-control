f2baa15edfb8a7f19dfc3a40e1e30eec9b4bc3d7
Raj Laud <raj.laud@gmail.com>
2021-10-07 16:11:54 -0400

    Move deployable files to own folder

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-scp -q * pi@media-pi.local:/srv/lms-bluetooth-control
+scp -q lms-bluetooth-control/* pi@media-pi.local:/srv/lms-bluetooth-control
 ssh pi@media-pi.local sudo systemctl restart lms-bluetooth-control
